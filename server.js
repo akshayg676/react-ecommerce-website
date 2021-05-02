ff29d014c7ad91fb13d88e0f9b72dc1325d8120e
Akshay G <akshayg676@gmail.com>
2021-05-02 09:23:53 +0530

    connect server and database successfully

---
+++ server.js
@@ -0,0 +1,41 @@
+require("dotenv").config();
+const express = require("express");
+const fileUpload = require("express-fileupload");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
+app.use(
+  fileUpload({
+    useTempFiles: true,
+  })
+);
+
+// Connect to mongodb
+const URI = process.env.MONGODB_URI;
+mongoose.connect(
+  URI,
+  {
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err) => {
+    if (err) throw err;
+    console.log("Connected to MongoDB");
+  }
+);
+
+app.get("/", (req, res) => {
+  res.json({ msg: "Welcome" });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log("Server is running");
+});
