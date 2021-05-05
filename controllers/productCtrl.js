36c54804569ed1acfdb2c9a0cc887ababfa90532
Akshay G <akshayg676@gmail.com>
2021-05-05 16:46:26 +0530

    complete product(2) filter,sort,pagination

--- controllers/productCtrl.js
+++ controllers/productCtrl.js
@@ -34,7 +34,7 @@ class APIfeatures {
 
   paginating() {
     const page = this.queryString.page * 1 || 1;
-    const limit = this.queryString.page * 1 || 9;
+    const limit = this.queryString.limit * 1 || 9;
     const skip = (page - 1) * limit;
     this.query = this.query.skip(skip).limit(limit);
     return this;
