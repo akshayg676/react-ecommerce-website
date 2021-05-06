204c62a73eb14572846d155fad084f407394f503
Akshay G <akshayg676@gmail.com>
2021-05-06 21:36:07 +0530

    complete skelton auth,cart,product and utils in mainpage

--- client/src/components/mainpages/Pages.js
+++ client/src/components/mainpages/Pages.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
+import Login from "./auth/Login";
+import Register from "./auth/Register";
+import Cart from "./cart/Cart";
+import Products from "./products/Products";
+import NotFound from "./utils/NotFound";
 
 function Pages() {
-  return <div>Main page</div>;
+  return (
+    <Switch>
+      <Route path="/" exact component={Products} />
+      <Route path="/login" exact component={Login} />
+      <Route path="/register" exact component={Register} />
+      <Route path="/cart" exact component={Cart} />
+      <Route path="*" exact component={NotFound} />
+    </Switch>
+  );
 }
 
 export default Pages;
---
+++ client/src/components/mainpages/cart/Cart.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Cart() {
+  return <div>cart</div>;
+}
+
+export default Cart;
