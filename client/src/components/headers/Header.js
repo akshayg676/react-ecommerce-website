721744d1d51db46a738482d1c3663a339fe8dc77
Akshay G <akshayg676@gmail.com>
2021-05-06 12:18:34 +0530

    complete client template

---
+++ client/src/components/headers/Header.js
@@ -0,0 +1,9 @@
+import React, { useState, useContext } from "react";
+import { GlobalState } from "../../GlobalState";
+
+function Header() {
+  const value = useContext(GlobalState);
+  return <div>{value}</div>;
+}
+
+export default Header;
---
+++ client/src/components/mainpages/Pages.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Pages() {
+  return <div>Main page</div>;
+}
+
+export default Pages;
