70cb4b550a4c95501ed281cc16f703f72ee7e4b2
Akshay G <akshayg676@gmail.com>
2021-05-08 23:49:22 +0530

    complete related products section

--- client/src/components/mainpages/detailProduct/DetailProduct.js
+++ client/src/components/mainpages/detailProduct/DetailProduct.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useContext } from "react";
 import { GlobalState } from "../../../GlobalState";
 import { useParams, Link } from "react-router-dom";
+import ProductItem from "../utils/productItem/ProductItem";
 
 function DetailProduct() {
   const params = useParams();
@@ -9,34 +10,48 @@ function DetailProduct() {
   const [detailProduct, setDetailProduct] = useState([]);
 
   useEffect(() => {
-    if (params) {
+    console.log("re render");
+    if (params.id) {
       products.forEach((product) => {
         if (product._id === params.id) {
           setDetailProduct(product);
         }
       });
     }
-  }, [params, products]);
+  }, [params.id, products]);
 
   if (detailProduct.length === 0) return null;
 
   return (
-    <div className="detail">
-      <img src={detailProduct.image.url} alt="product image" />
-      <div className="box-detail">
-        <div className="row">
-          <h2>{detailProduct.title}</h2>
-          <h4>{detailProduct.product_id}</h4>
+    <>
+      <div className="detail">
+        <img src={detailProduct.image.url} alt="product image" />
+        <div className="box-detail">
+          <div className="row">
+            <h2>{detailProduct.title}</h2>
+            <h4>{detailProduct.product_id}</h4>
+          </div>
+          <span>₹ {detailProduct.price}</span>
+          <p>{detailProduct.description}</p>
+          <p>{detailProduct.content}</p>
+          <p>Sold: {detailProduct.sold}</p>
+          <Link to="/cart" className="cart">
+            Buy Now
+          </Link>
         </div>
-        <span>₹ {detailProduct.price}</span>
-        <p>{detailProduct.description}</p>
-        <p>{detailProduct.content}</p>
-        <p>Sold: {detailProduct.sold}</p>
-        <Link to="/cart" className="cart">
-          Buy Now
-        </Link>
       </div>
-    </div>
+
+      <div>
+        <h2>Related Products</h2>
+        <div className="products">
+          {products.map((product) => {
+            return product.category === detailProduct.category ? (
+              <ProductItem key={product._id} product={product} />
+            ) : null;
+          })}
+        </div>
+      </div>
+    </>
   );
 }
 
