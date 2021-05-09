26d458bfb675c7be8414d961bd6f87e662858c2a
Akshay G <akshayg676@gmail.com>
2021-05-09 15:28:41 +0530

    complete login.js

--- client/src/components/mainpages/auth/Login.js
+++ client/src/components/mainpages/auth/Login.js
@@ -1,7 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 function Login() {
-  return <div>Login</div>;
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+
+  const loginSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post("/user/login", { ...user });
+
+      localStorage.setItem("firstLogin", true);
+
+      window.location.href = "/";
+    } catch (err) {
+      alert(err.response.data.msg);
+    }
+  };
+  return (
+    <div className="login-page">
+      <form onSubmit={loginSubmit}>
+        <input
+          type="email"
+          name="email"
+          required
+          value={user.email}
+          placeholder="Email"
+          onChange={(e) => {
+            setUser({ ...user, email: e.target.value });
+          }}
+        />
+        <input
+          type="password"
+          name="password"
+          required
+          value={user.password}
+          placeholder="Password"
+          onChange={(e) => {
+            setUser({ ...user, password: e.target.value });
+          }}
+        />
+
+        <div className="row">
+          <button type="submit">Login</button>
+          <Link to="register">Register</Link>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default Login;
