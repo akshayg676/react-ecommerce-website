bffee08541c695f134c0f2631015ef5d1c56942b
Akshay G <akshayg676@gmail.com>
2021-05-14 12:58:51 +0530

    update login and register route

--- client/src/components/mainpages/Pages.js
+++ client/src/components/mainpages/Pages.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Switch, Route } from "react-router-dom";
 import Login from "./auth/Login";
 import Register from "./auth/Register";
@@ -7,13 +7,21 @@ import Products from "./products/Products";
 import DetailProduct from "./detailProduct/DetailProduct";
 import NotFound from "./utils/NotFound";
 
+import { GlobalState } from "../../GlobalState";
+
 function Pages() {
+  const state = useContext(GlobalState);
+  const [isLogged] = state.userAPI.isLogged;
   return (
     <Switch>
       <Route path="/" exact component={Products} />
       <Route path="/detail/:id" exact component={DetailProduct} />
-      <Route path="/login" exact component={Login} />
-      <Route path="/register" exact component={Register} />
+      <Route path="/login" exact component={isLogged ? NotFound : Login} />
+      <Route
+        path="/register"
+        exact
+        component={isLogged ? NotFound : Register}
+      />
       <Route path="/cart" exact component={Cart} />
       <Route path="*" exact component={NotFound} />
     </Switch>
