31dcdae47fc972b1999d4b2bbdb48b0cb6e80242
Akshay G <akshayg676@gmail.com>
2021-07-21 12:29:52 +0530

    update login/register card

--- client/src/components/footer/Footer.js
+++ client/src/components/footer/Footer.js
@@ -1,38 +1,38 @@
 import "./footer.css";
-import { FaTelegram, FaCodepen } from "react-icons/fa";
+import { FaTelegram, FaCodepen, FaGlobe } from "react-icons/fa";
 import { GrLinkedin } from "react-icons/gr";
 import { GoMarkGithub } from "react-icons/go";
 import { MdEmail } from "react-icons/md";
 
+const iconStyle = {
+  color: "black",
+  margin: " 0 15px",
+};
+
 function Footer() {
   return (
     <div className="footer">
-      <div className="about">
-        <h2>About</h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae
-          voluptates non ipsam ea incidunt vel hic mollitia cum commodi harum
-        </p>
-      </div>
       <section class="social">
         <div class="social-container">
           <a href="#">
-            <GrLinkedin />
+            <FaGlobe style={iconStyle} />
           </a>
           <a href="#">
-            <GoMarkGithub />
+            <GrLinkedin style={iconStyle} />
+          </a>
+
+          <a href="#">
+            <GoMarkGithub style={iconStyle} />
           </a>
           <a href="#">
-            <FaCodepen />
+            <FaCodepen style={iconStyle} />
           </a>
           <a href="#">
-            <MdEmail />
-          </a>
-          <a href="#">
-            <FaTelegram />
+            <MdEmail style={iconStyle} />
           </a>
         </div>
       </section>
+      <div className="copyright">© 2021 | All rights reserved</div>
     </div>
   );
 }
